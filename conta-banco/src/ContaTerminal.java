881e5d29ab9e2efd6fd3ee359164dc9ab268630e
Damares Gaia <damaresgaia@gmail.com>
2025-02-13 21:00:43 -0300

    feat: implementa captura e exibição de dados bancários no ContaBanco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,31 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // Criando o objeto Scanner para ler os dados do terminal
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitando e lendo as informações fornecidas pelo usuário
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = scanner.nextLine(); // Leitura da agência
+
+        System.out.println("Por favor, digite o número da Conta !");
+        int numeroConta = scanner.nextInt(); // Leitura do número da conta
+
+        scanner.nextLine(); // Consumindo o newline deixado por nextInt()
+
+        System.out.println("Por favor, digite o Nome do Cliente !");
+        String nomeCliente = scanner.nextLine(); // Leitura do nome do cliente
+
+        System.out.println("Por favor, digite o saldo da conta !");
+        double saldo = scanner.nextDouble(); // Leitura do saldo
+
+        // Exibindo a mensagem final
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo
+                + " já está disponível para saque.");
+
+        // Fechando o scanner
+        scanner.close();
     }
 }
